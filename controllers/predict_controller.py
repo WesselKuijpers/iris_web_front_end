08de8fddb00911ed39964e24b180dc0d04ea210b
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-04-11 13:23:54 +0200

    code cleanup

--- controllers/predict_controller.py
+++ controllers/predict_controller.py
@@ -1,49 +1,42 @@
-from flask import Flask, Blueprint, jsonify
-from flask import render_template
-from helpers import predict_helper
-from flask import abort
+from flask import Blueprint, Flask, abort
 from flask import current_app as app
-import tensorflow as tf
-import keras.backend as K
+from flask import jsonify, render_template, request
+
+from helpers import predict_helper
 
 predict_controller = Blueprint('predict_controller', __name__)
 
-class PredictController:
-    def __init__(self):
-        # TODO: make this dynamic
-        self.classes = [
-            'Cox',
-            'Elstar',
-            'Golden Delicious',
-            'Granny Smith',
-            'Nashi Pear',
-            'Pink Lady'
-        ]
+# TODO: find some better way to do this
+classes = [
+    'Cox',
+    'Elstar',
+    'Golden Delicious',
+    'Granny Smith',
+    'Nashi Pear',
+    'Pink Lady'
+]
 
-    # @predict_controller.route('/', methods=['POST'])
-    def predict_index(self, request, model, graph):
-        try:
-            helper = predict_helper.PredictHelper()
+@predict_controller.route('/', methods=['POST'])
+def predict_index():
+    try:
+        helper = predict_helper.PredictHelper()
 
-            # get the actual model from the local storage by the location in the model object
-            # model = helper.load_model('iris_core/densenet.h5py')
+        # get the actual model from the running application
+        model = app.helper.model
 
-            # get and reshape the image from the form data
-            image = request.files['image']
-            image = helper.reshape_image(image)
+        # get and reshape the image from the form data
+        image = request.files['image']
+        image = helper.reshape_image(image)
 
-            # make a prediction and make a collection for every item in it
-            with graph.as_default():
-                raw_prediction = model.predict_classes(image[0])
+        # make a prediction and make a collection for every item in it
+        with app.helper.graph.as_default():
+            raw_prediction = model.predict_classes(image[0], verbose=True)
 
-            return jsonify(self.classes[raw_prediction[0]], image[1])
-        except FileNotFoundError:
-            # abort(404)
-            raise
-        except ValueError:
-            raise
-            # abort(500)
-        except:
-            raise
-            # abort(503)
-        
+        return jsonify(classes[raw_prediction[0]], image[1])
+    except FileNotFoundError:
+        abort(404)
+    except ValueError:
+        abort(500)
+    except:
+        raise
+        # abort(503)
--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-from flask import Flask, Blueprint, request
-from controllers.page_controller import page_controller
-# from controllers.predict_controller import predict_controller
-from helpers.predict_helper import PredictHelper
-from flask import g
-from controllers.predict_controller import PredictController
-import tensorflow as tf
 import keras.backend as K
+import tensorflow as tf
+from flask import Blueprint, Flask, g
+
+from controllers.page_controller import page_controller
+from controllers.predict_controller import predict_controller
+from helpers.predict_helper import PredictHelper
 
 app = Flask(__name__)
 
-global graph
-graph = tf.get_default_graph()
-model = PredictHelper().load_model('iris_core/densenet.h5py')
+# pre-load the helper class / model
+app.helper = PredictHelper()
+app.helper.load_model('iris_core/densenet.h5py')
 print("LOADING: DONE")
 
+# register the blueprints
+# TODO: find some dynamic way to do this
 app.register_blueprint(page_controller, url_prefix="/")
-# app.register_blueprint(predict_controller, url_prefix="/predict")
-
-@app.route('/predict/', methods=['POST'])
-def predict():
-    controller = PredictController()
-    result = controller.predict_index(request, model, graph)
-    return result
+app.register_blueprint(predict_controller, url_prefix="/predict")

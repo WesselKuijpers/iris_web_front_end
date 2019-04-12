8c618641dcc552bdd8b2a891120264fb525e9c3c
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-04-12 15:22:59 +0200

    previously cached files are now being removed on server start

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from controllers.page_controller import page_controller
 from controllers.predict_controller import predict_controller
 from helpers.predict_helper import PredictHelper
 
+import os
+
 app = Flask(__name__)
 
 # pre-load the helper class / model
 app.helper = PredictHelper()
 app.helper.load_model('iris_core/densenet.h5py')
 print("LOADING: DONE")
+
+# cleanup cached files
+path = 'static/img/cache/'
+filelist = os.listdir(path)
+for f in filelist:
+    os.remove(os.path.join(path, f))
 
 # register the blueprints
 # TODO: find some dynamic way to do this

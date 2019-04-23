430d9c335b166a62d12dbccdd42d51dc5a7acf26
Wessel Kuijpers <wrkuijpers@outlook.com>
2019-04-23 11:26:14 +0200

    model now trains asynchronously while server runs

--- fruit_iris_core/boot/server.py
+++ fruit_iris_core/boot/server.py
@@ -13,11 +13,15 @@ from helpers.predict_helper import PredictHelper
 
 class Server:
     def __init__(self):
+        # specify directory for template and static files
         template_dir = os.path.abspath('templates')
         static_dir = os.path.abspath('static')
+
+        # initiate the app
         self.app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
-    def boot(self):
+    def start(self):
+        # pipeline for calling all methods
         try:
             self.load_helper()
             self.clean_cache()
@@ -30,8 +34,12 @@ class Server:
             raise
 
     def load_helper(self):
+        # instantiate the predicthelper class
         self.app.helper = PredictHelper()
+        # load the model
         self.app.helper.load_model('fruit_iris_core/densenet.h5py')
+
+        # if the model is loaded, let the sun shine, else let the user know an error occured
         if(self.app.helper.model):
             print("MODEL: LOADED")
             return True
@@ -41,11 +49,14 @@ class Server:
             return False
 
     def clean_cache(self):
+        # clean all the images that were saved for prediction purposes
         path = 'static/img/cache/'
         filelist = os.listdir(path)
         for f in filelist:
             os.remove(os.path.join(path, f))
             print("FILE '" + path + f + "' DESTROYED")
+
+        # if the clean action is succesfull, continue, else return error
         if(len(filelist) == 0):
             print("FILECACHE: CLEAN")
             return True
@@ -55,11 +66,12 @@ class Server:
             return False
         
     def register_blueprints(self):
+        # register all the required functional blueprints pointing to the controllers
         # TODO: find some dynamic way to do this
         self.app.register_blueprint(page_controller, url_prefix="/")
         self.app.register_blueprint(predict_controller, url_prefix="/predict")
         self.app.register_blueprint(rate_controller, url_prefix='/rate')
 
     def register_error_handlers(self):
-        # TODO: find some dynamic way to do this
+        # register the error handler
         self.app.register_blueprint(errors)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fruit_iris_core.boot.server import Server
+from fruit_iris_core.boot.trainer import Trainer
+import multiprocessing as mp
 
-app = Server().boot()
+output = mp.Queue()
+
+train = Trainer(epochs=5, batch_size=32, train_dir='dataset/train', val_dir='dataset/test', width=224, height=224)
+
+# server_process = mp.Process(target=server.start)
+# train_process = mp.Process(target=train.start)
+
+app = Server().start()
+train.start()
